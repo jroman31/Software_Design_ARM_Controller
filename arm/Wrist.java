1424a177dbe3a34f2ddd269a79edd4094dabd214
roman <roman@10.85.49.115>
2019-11-16 17:26:54 -0800

    test

---
+++ arm/Wrist.java
@@ -0,0 +1,24 @@
+package arm;
+public class Wrist {	
+	int wristJoint;
+	
+	public Wrist(int wristJoint) {
+		this.wristJoint=wristJoint;
+	}
+	
+	public int getWristJoint() {
+		return wristJoint;
+	}
+
+	public void setWristJoint(int wristJoint) {
+		this.wristJoint = wristJoint;
+	}
+
+	public void wristGoTo(int theta) {
+		this.wristJoint=theta;
+	}
+	
+	public String toString() {
+		return  "," + wristJoint+","; 
+	}
+}
